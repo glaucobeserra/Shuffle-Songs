000fc76ba9aaac293b0c96e89d27f5565b72725d
Glauco Dantas Beserra <dantas.glauco@gmail.com>
2019-10-06 09:49:25 -0300

    Project created

---
+++ Shuffle Songs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shuffle Songs
+//
+//  Created by Glauco Dantas Beserra on 06/10/19.
+//  Copyright © 2019 Glauco Dantas Beserra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
